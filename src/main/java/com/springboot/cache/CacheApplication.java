be25ecc93dff4c0efed94b0f237dccb28a171904
qiaoyn <1750718285@qq.com>
2019-11-20 09:28:14 +0800

    开启缓存注解

--- src/main/java/com/springboot/cache/CacheApplication.java
+++ src/main/java/com/springboot/cache/CacheApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+*
+* 用来演示springboot缓存
+* @param
+* @author qiaoyn
+*/
+
 @SpringBootApplication
 @EnableCaching  //开启缓存注解
 public class CacheApplication {
